27e7f2ce7c2add3727e4cf71909fcb10317b127e
jhamilcali <jhamilquea@gmail.com>
2025-08-02 11:02:52 -0400

    conexion a wallwt y sacando adres

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,32 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'wallet_provider.dart';
+import 'package:flutter_metamask/metamask.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (_) => WalletProvider(),
-      child: MaterialApp(
-        title: 'MetaMask Flutter Android',
-        theme: ThemeData.dark(),
-        home: const WalletPage(),
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
       ),
-    );
-  }
-}
-
-class WalletPage extends StatelessWidget {
-  const WalletPage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    final provider = Provider.of<WalletProvider>(context);
-    return Scaffold(
-      appBar: AppBar(title: const Text('MetaMask + Flutter')),
-      body: Center(
-        child:
-            provider.isConnected
-                ? Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    const Text("Wallet conectada"),
-                    Text(provider.walletAddress ?? ''),
-                    const SizedBox(height: 20),
-                    ElevatedButton(
-                      onPressed: () => provider.disconnect(),
-                      child: const Text("Desconectar"),
-                    ),
-                  ],
-                )
-                : ElevatedButton(
-                  onPressed: () => provider.connectWallet(),
-                  child: const Text("Conectar MetaMask"),
-                ),
-      ),
+      home: MatamaskScreen(),
     );
   }
 }
